52a8b5d5a6b2d922fdf9b16bd61e3983c0da0f39
Administrator <Administrator@304-3>
2022-12-07 11:17:06 +0900

    todayHouse

---
+++ src/main/java/com/green/nowon/TodayHouseApplication.java
@@ -0,0 +1,13 @@
+package com.green.nowon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayHouseApplication.class, args);
+	}
+
+}
